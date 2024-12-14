44478a90b1f70343bbff933c2d27bb5bd14c345f
danielRamosMencia <danieldermramos@gmail.com>
2024-12-14 11:42:08 -0600

    feat: logout controller

---
+++ internal/controllers/auth/logout.go
@@ -0,0 +1,20 @@
+package authcontrollers
+
+import (
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func Logout(c *fiber.Ctx) error {
+	expired := time.Now().Add(-time.Hour * 24)
+	c.Cookie(&fiber.Cookie{
+		Name:    "token",
+		Value:   "",
+		Expires: expired,
+	})
+
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"mensaje": "Sesión cerrada con éxito",
+	})
+}
