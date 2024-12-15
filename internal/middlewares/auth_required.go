2256aa02728b11d867c34c79971f0fac804c9b5c
danielRamosMencia <danieldermramos@gmail.com>
2024-12-15 13:19:12 -0600

    feat: authentication middleware

---
+++ internal/middlewares/auth_required.go
@@ -0,0 +1,60 @@
+package middlewares
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/golang-jwt/jwt/v5"
+)
+
+func AuthRequired(c *fiber.Ctx) error {
+	var stringToken string
+	authorization := c.Get("Authorization")
+
+	if strings.HasPrefix(authorization, "Bearer ") {
+		stringToken = strings.TrimPrefix(authorization, "Bearer ")
+	} else if c.Cookies("token") != "" {
+		stringToken = c.Cookies("token")
+	}
+
+	if stringToken == "" {
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"mensaje": "Inautorizado",
+			"código":  "middle-err-000",
+		})
+	}
+
+	jwtSecret := os.Getenv("JWT_SECRET")
+
+	byteToken, err := jwt.Parse(stringToken, func(jwtToken *jwt.Token) (interface{}, error) {
+		if _, ok := jwtToken.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexepected sigining method: %s", jwtToken.Header["alg"])
+		}
+
+		return []byte(jwtSecret), nil
+	})
+	if err != nil {
+		log.Println("Error verifying token: ", err)
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"mensaje": "Inautorizado",
+			"código":  "middle-err-001",
+		})
+	}
+
+	claims, ok := byteToken.Claims.(jwt.MapClaims)
+	if !ok || !byteToken.Valid {
+		log.Println("Error verifying token claims: ", err)
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"mensaje": "Inautorizado",
+			"código":  "middle-err-002",
+		})
+	}
+
+	// TODO verify user
+	log.Println(claims["id"])
+
+	return c.Next()
+}
